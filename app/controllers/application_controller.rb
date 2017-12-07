bcf4e50254f80dc85b117994caf3d4f7b51ff8d4
dcgmelton <dcgmelton>
2017-12-07 14:28:26 -0600

    Improve the README file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 def hello	
-render html: "hello, world!"
+render html: "I Love GITHUB!"
   end
 end
